454b1789fac92ada606af9e0c4558fd80d4bb291
Futa Ogawa <ogawa0071@gmail.com>
2019-01-12 00:45:58 +0900

    Migrate to Nuxt.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,55 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'ナブラ演算子ゲーム',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { property: 'og:url', content: 'https://nablagame.com/' },
+      { property: 'og:title', content: 'ナブラ演算子ゲーム' },
+      {
+        property: 'og:description',
+        content:
+          'ナブラ演算子ゲームは「∇（ナブラ演算子）」などの演算子を使って、基底を微分して相手を0次元にするボードゲームです。'
+      },
+      { property: 'og:image', content: 'https://nablagame.com/og.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@nablagame' },
+      { name: 'twitter:title', content: 'ナブラ演算子ゲーム' },
+      {
+        name: 'twitter:description',
+        content:
+          'ナブラ演算子ゲームは「∇（ナブラ演算子）」などの演算子を使って、基底を微分して相手を0次元にするボードゲームです。'
+      },
+      { name: 'twitter:image', content: 'https://nablagame.com/og.png' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', href: '/favicon.png' },
+      { rel: 'canonical', href: 'https://nablagame.com/' }
+    ],
+    script: [
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=UA-44434462-3',
+        async: ''
+      }
+    ],
+    link: [
+      // { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.6.4/jquery.fullPage.min.css' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700'
+      }
     ]
   },
 
@@ -26,20 +67,17 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
+  modules: [],
 
   /*
   ** Build configuration
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,170 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nablagame.com
-      </h1>
-      <h2 class="subtitle">
-        My bedazzling Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div id="fullpage">
+    <section class="section intro">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <h1 class="brand-heading">Nabla Game</h1>
+            <p class="intro-text">ナブラ演算子ゲーム</p>
+            <a 
+              href="#about" 
+              class="btn btn-circle page-scroll">
+              <i class="fa fa-angle-double-down animated"/>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+    <section class="section about">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
+            <h1>About</h1>
+            <p>
+              <strong>「∇」という記号をご存じでしょうか？</strong><br>
+              <br>
+              「∇」は、ナブラ演算子といい、数学、物理、化学などで使う微分演算子です。<br>
+              このように聞くと、難しく感じられる方も多いかも知れませんが、この演算子を気楽に楽しめるゲームがあるとしたらどうでしょう。<br>
+              <br>
+              それが「ナブラ演算子ゲーム」です。ルールはいたってシンプル。基底を微分して相手を0次元にするだけです。<br>
+              <br>
+              一度遊び方を覚えれば、いつでもこの刺激的なゲームをプレイすることができます。<br>
+              <br>
+              是非とも「ナブラ演算子ゲーム」の世界に足を踏み入れてみて下さい。<br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section play">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
+            <h1>How to Play</h1>
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/kf0DAygsXAU" 
+                frameborder="0" 
+                allowfullscreen/>
+            </div>
+            <ul class="list-inline banner-social-buttons pdf">
+              <li>
+                <a 
+                  href="/how_to_play.pdf" 
+                  target="_blank" 
+                  class="btn btn-default btn-lg">
+                  <i class="fa fa-file-pdf-o fa-fw"/>
+                  <span class="network-name">PDF</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section buy">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
+            <h1>How to Buy</h1>
+            <p>
+              現在「ナブラ演算子ゲーム」は、東京大学生協第二購買部（本郷キャンパス）および<a 
+                href="http://www.utcoop-netshop.jp/249_732.html" 
+                target="_blank">東京大学生協通販サイト</a>にて販売しております。<br>
+              今後の一般販売やイベントなどの最新情報については、随時公式Twitterでも掲載いたします。<br>
+            </p>
+            <ul class="list-inline banner-social-buttons twitter">
+              <li>
+                <a 
+                  href="https://twitter.com/nablagame" 
+                  target="_blank" 
+                  class="btn btn-default btn-lg">
+                  <i class="fa fa-twitter fa-fw"/>
+                  <span class="network-name">Twitter</span>
+                </a>
+              </li>
+            </ul>
+            <p>
+              ナブラ演算子ゲームについてのお問い合わせはこちらまで<br>
+              <a href="mailto:nablaoperatorgame@gmail.com">nablaoperatorgame@gmail.com</a><br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import $ from 'jquery'
+import fullpage from 'fullpage.js'
 
-export default {
-  components: {
-    Logo
-  }
-}
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    paddingTop: '0px',
+    paddingBottom: '0px',
+    anchors: ['intro', 'about', 'play', 'buy'],
+    navigation: true
+  })
+})
+
+export default {}
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style lang="scss">
+@import '~/node_modules/fullpage.js/dist/jquery.fullpage.css';
+</style>
+
+<style lang="scss" scoped>
+@import '~/assets/_grayscale.scss';
+
+body {
+  color: $black;
+  background-color: $white;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.intro {
+  color: $black;
+  font-weight: bold;
+  background: url('~assets/math.png') repeat;
+  background-color: $white;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.btn-circle {
+  border: 2px solid $black;
+  color: $black;
+  &:focus {
+    color: $black;
+    background: rgba($black, 0.1);
+  }
 }
 
-.links {
-  padding-top: 15px;
+.pdf {
+  margin-top: 50px;
+}
+
+.twitter {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+.brand-heading {
+  font-size: 40px;
+  @media (min-width: 768px) {
+    font-size: 100px;
+  }
+}
+
+.intro-text {
+  font-size: 18px;
+  @media (min-width: 768px) {
+    font-size: 26px;
+  }
+}
+
+.buy {
+  background: url('~assets/congruent_pentagon.png') repeat;
 }
 </style>
