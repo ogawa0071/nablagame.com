e77454aaa0f15759e9bfe44c91b965bdee539239
Futa Ogawa <ogawa0071@gmail.com>
2019-08-22 17:59:58 +0900

    「How to Buy」を更新

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
             <a 
               href="#about" 
               class="btn btn-circle page-scroll">
-              <i class="fa fa-angle-double-down animated"/>
+              <i class="fa fa-angle-double-down animated" />
             </a>
           </div>
         </div>
@@ -21,16 +21,17 @@
           <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
             <h1>About</h1>
             <p>
-              <strong>「∇」という記号をご存じでしょうか？</strong><br>
-              <br>
-              「∇」は、ナブラ演算子といい、数学、物理、化学などで使う微分演算子です。<br>
-              このように聞くと、難しく感じられる方も多いかも知れませんが、この演算子を気楽に楽しめるゲームがあるとしたらどうでしょう。<br>
-              <br>
-              それが「ナブラ演算子ゲーム」です。ルールはいたってシンプル。基底を微分して相手を0次元にするだけです。<br>
-              <br>
-              一度遊び方を覚えれば、いつでもこの刺激的なゲームをプレイすることができます。<br>
-              <br>
-              是非とも「ナブラ演算子ゲーム」の世界に足を踏み入れてみて下さい。<br>
+              <strong>「∇」という記号をご存じでしょうか？</strong>
+              <br >
+              <br >「∇」は、ナブラ演算子といい、数学、物理、化学などで使う微分演算子です。
+              <br >このように聞くと、難しく感じられる方も多いかも知れませんが、この演算子を気楽に楽しめるゲームがあるとしたらどうでしょう。
+              <br >
+              <br >それが「ナブラ演算子ゲーム」です。ルールはいたってシンプル。基底を微分して相手を0次元にするだけです。
+              <br >
+              <br >一度遊び方を覚えれば、いつでもこの刺激的なゲームをプレイすることができます。
+              <br >
+              <br >是非とも「ナブラ演算子ゲーム」の世界に足を踏み入れてみて下さい。
+              <br >
             </p>
           </div>
         </div>
@@ -42,12 +43,13 @@
           <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
             <h1>How to Play</h1>
             <div class="embed-responsive embed-responsive-16by9">
-              <iframe 
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/embed/kf0DAygsXAU" 
-                frameborder="0" 
-                allowfullscreen/>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/kf0DAygsXAU"
+                frameborder="0"
+                allowfullscreen
+              />
             </div>
             <ul class="list-inline banner-social-buttons pdf">
               <li>
@@ -55,7 +57,7 @@
                   href="/how_to_play.pdf" 
                   target="_blank" 
                   class="btn btn-default btn-lg">
-                  <i class="fa fa-file-pdf-o fa-fw"/>
+                  <i class="fa fa-file-pdf-o fa-fw" />
                   <span class="network-name">PDF</span>
                 </a>
               </li>
@@ -69,26 +71,58 @@
         <div class="row">
           <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
             <h1>How to Buy</h1>
+            <h2>店舗</h2>
             <p>
-              現在「ナブラ演算子ゲーム」は、東京大学生協第二購買部（本郷キャンパス）および<a 
-                href="http://www.utcoop-netshop.jp/249_732.html" 
-                target="_blank">東京大学生協通販サイト</a>にて販売しております。<br>
-              今後の一般販売やイベントなどの最新情報については、随時公式Twitterでも掲載いたします。<br>
+              <strong>東京大学生協購買部</strong>
+              <br >- 本郷キャンパス第二購買部
+              <br >- 駒場キャンパス購買部
+              <br >
+            </p>
+            <p>
+              <strong>東京理科大学生協購買部</strong>
+              <br >- 神楽坂店
+              <br >- 野田店
+              <br >
+            </p>
+            <p>
+              <strong>
+                <a 
+                  href="https://sugorokuya.jp/" 
+                  target="_blank">すごろくや</a>
+              </strong>
+              <br >
+            </p>
+            <h2>オンラインショップ</h2>
+            <p>
+              <strong>
+                <a 
+                  href="https://sugorokuya.jp/gamelist?s=m:ナブラ演算子ゲーム製作委員会" 
+                  target="_blank">すごろくや</a>
+              </strong>
+              <br >
+            </p>
+            <p>
+              現在「ナブラ演算子ゲーム」は、定価1,400円＋税のところ、各大学生協では特別価格1,200円＋税で販売しております。
+              <br >在庫状況や今後のイベントなどの最新情報については、随時公式Twitterでも掲載いたします。
+              <br >
             </p>
             <ul class="list-inline banner-social-buttons twitter">
               <li>
-                <a 
-                  href="https://twitter.com/nablagame" 
-                  target="_blank" 
-                  class="btn btn-default btn-lg">
-                  <i class="fa fa-twitter fa-fw"/>
+                <a
+                  href="https://twitter.com/nablagame"
+                  target="_blank"
+                  class="btn btn-default btn-lg"
+                >
+                  <i class="fa fa-twitter fa-fw" />
                   <span class="network-name">Twitter</span>
                 </a>
               </li>
             </ul>
             <p>
-              ナブラ演算子ゲームについてのお問い合わせはこちらまで<br>
-              <a href="mailto:nablaoperatorgame@gmail.com">nablaoperatorgame@gmail.com</a><br>
+              ナブラ演算子ゲームについてのお問い合わせはこちらまで
+              <br >
+              <a href="mailto:nablaoperatorgame@gmail.com">nablaoperatorgame@gmail.com</a>
+              <br >
             </p>
           </div>
         </div>
